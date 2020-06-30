08090762cbe027b010b293e243ae9312fd5aaa2a
Ivan Shalganov <a.cobest@gmail.com>
2020-06-30 22:17:58 +0300

    Fix travis build

--- .travis/before.sh
+++ .travis/before.sh
@@ -6,4 +6,4 @@ luarocks install busted
 luarocks install cluacov
 luarocks install lua-cjson $LUA_CJSON
 # lua 5.3+ already has utf8 module
-lua -e 'if utf8 then os.exit(1) else os.exit(0) end' && luarocks install utf8; true
+luarocks install utf8; true
